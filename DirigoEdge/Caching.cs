d4143292c049524558707501c0524ecca43f57a6
unknown <cbelanger@dir-cbelanger.WS08Domain01.local>
2015-02-24 11:52:42 -0500

    interim checkin

---
+++ DirigoEdge/Caching.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using AutoMapper;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace DirigoEdge
+{
+	public static class Caching
+	{
+		private static ConnectionMultiplexer _redis;
+
+		public static ConnectionMultiplexer Redis
+		{
+			get { return _redis ?? (_redis = ConnectionMultiplexer.Connect("localhost")); }
+		}
+
+		public static IDatabase Cache
+		{
+			get { return Redis.GetDatabase(); }
+		}
+	}
+
+	public abstract class CacheableObject
+	{
+		public abstract void Load();
+
+		public String Key;
+		private Boolean Loaded = false;
+
+		protected CacheableObject(object key)
+		{
+			
+
+			try
+			{
+				if (key == null)
+				{
+					key = GetType() + "_undefined_key";
+				}
+
+				Key = key.ToString();
+
+				var cachedString = Caching.Cache.StringGet(Key);
+
+				if (String.IsNullOrEmpty(cachedString))
+				{
+					Load();
+					Loaded = true;
+					var value = Serialize();
+					Caching.Cache.StringSet(Key, value);
+				}
+				else
+				{
+					var type = GetType();
+					var deserializedObject = Deserialize(cachedString);
+
+					Mapper.CreateMap(type, type);
+					Mapper.Map(deserializedObject, this);
+				}
+			}
+			catch (Exception ex)
+			{
+				if (!Loaded)
+				{
+					Load();
+				}
+
+			}
+		}
+
+		protected String Serialize()
+		{
+			return JsonConvert.SerializeObject(this);
+		}
+
+		protected object Deserialize(String serializedString)
+		{
+			return JsonConvert.DeserializeObject(serializedString, GetType());
+		}
+	}
+}
--- DirigoEdge/Models/ViewModels/BlogHomeViewModel.cs
+++ DirigoEdge/Models/ViewModels/BlogHomeViewModel.cs
@@ -4,65 +4,68 @@ using System.Linq;
 using DirigoEdgeCore.Data.Entities;
 using DirigoEdgeCore.Models;
 using DirigoEdgeCore.Models.ViewModels;
+using DirigoEdgeCore.Utils;
 
 namespace DirigoEdge.Models.ViewModels
 {
-    public class BlogHomeViewModel : DirigoBaseModel
-    {
-        public List<Blog> BlogRoll;
-        public List<Blog> AllBlogs;
-        public BlogsCategoriesViewModel BlogCats;
+	public class BlogHomeViewModel : CacheableObject
+	{
+		public List<Blog> BlogRoll;
+		public List<Blog> AllBlogs;
+		public BlogsCategoriesViewModel BlogCats;
 
-        public int BlogRollCount;
-        public int MaxBlogCount;
-        public int LastBlogId;
-        public int SkipBlogs;
+		public int BlogRollCount;
+		public int MaxBlogCount;
+		public int LastBlogId;
+		public int SkipBlogs;
 
-        public Blog FeaturedBlog;
-        public bool ReachedMaxBlogs;
+		public Blog FeaturedBlog;
+		public bool ReachedMaxBlogs;
 
-        public string CurrentMonth;
+		public string CurrentMonth;
 
-        public string BlogTitle;
+		public string BlogTitle;
 
-        public BlogHomeViewModel(string date = "")
-        {
-            var model = new BlogListModel(Context);
-            MaxBlogCount = model.GetBlogSettings().MaxBlogsOnHomepageBeforeLoad;
-            SkipBlogs = MaxBlogCount;
+		public BlogHomeViewModel(object key)
+			: base(key)
+		{
 
-            BlogTitle = model.GetBlogSettings().BlogTitle;
+		}
 
-            FeaturedBlog = Context.Blogs.FirstOrDefault(x => x.IsFeatured);
+		public override void Load()
+		{
+			var model = new BlogListModel(Context);
+			MaxBlogCount = model.GetBlogSettings().MaxBlogsOnHomepageBeforeLoad;
+			
+			SkipBlogs = MaxBlogCount;
+			BlogTitle = model.GetBlogSettings().BlogTitle;
+			FeaturedBlog = Context.Blogs.FirstOrDefault(x => x.IsFeatured);
+			AllBlogs = Context.Blogs.Where(x => x.IsActive).ToList();
+			BlogCats = new BlogsCategoriesViewModel("");
 
-            CurrentMonth = "";
+			DateTime startDate;
+			if (!String.IsNullOrEmpty(Key) && DateTime.TryParse(Key, out startDate))
+			{
+				CurrentMonth = startDate.ToString("MM/yyyy");
 
-            AllBlogs = Context.Blogs.Where(x => x.IsActive).ToList();
-
-            BlogRoll = AllBlogs.Where(x => x.IsActive)
-                        .OrderByDescending(x => x.Date)
-                        .Take(MaxBlogCount)
-                        .ToList();
-
-            BlogCats = new BlogsCategoriesViewModel("");
-
-            if (!String.IsNullOrEmpty(date))
-            {
-                DateTime startDate = Convert.ToDateTime(date);
-
-                CurrentMonth = startDate.ToString("MM/yyyy");
-
-                BlogRoll =
-                    Context.Blogs.Where(
-                        x => x.IsActive
-                             && (x.Date.Month == startDate.Month)
-                             && (x.Date.Year == startDate.Year)
-                        )
-                           .OrderByDescending(x => x.Date)
-                           .Take(MaxBlogCount)
-                           .ToList();
-            }
-
-        }
-    }
+				BlogRoll =
+					Context.Blogs.Where(
+						x => x.IsActive
+						     && (x.Date.Month == startDate.Month)
+						     && (x.Date.Year == startDate.Year)
+						)
+						.OrderByDescending(x => x.Date)
+						.Take(MaxBlogCount)
+						.ToList();
+			}
+			else
+			{
+				CurrentMonth = "";
+				BlogRoll = AllBlogs.Where(x => x.IsActive)
+						.OrderByDescending(x => x.Date)
+						.Take(MaxBlogCount)
+						.ToList();
+			}
+		}
+	}
 }
