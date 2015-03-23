e9feb26f99b1410d227716963642f1ef0fdb658d
justincolangelo@gmail.com <justincolangelo@gmail.com>
2015-03-23 15:45:06 -0400

    filter by schema in module listing admin page

--- DirigoEdge/Areas/Admin/Models/ViewModels/ManageModulesViewModel.cs
+++ DirigoEdge/Areas/Admin/Models/ViewModels/ManageModulesViewModel.cs
@@ -8,11 +8,13 @@ namespace DirigoEdge.Areas.Admin.Models.ViewModels
     public class ManageModulesViewModel : DirigoBaseModel
     {
         public List<ContentModule> Modules;
+        public List<Schema> Schemata; 
 
         public ManageModulesViewModel()
         {
             BookmarkTitle = "Content Modules";
             Modules = Context.ContentModules.Where(mod => mod.ParentContentModuleId == null).ToList();
+            Schemata = Context.Schemas.OrderBy(x => x.DisplayName).ToList();
         }
     }
 }
